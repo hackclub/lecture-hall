e761a381843480a8e833da6cf3477ff47248caa7
Max Wofford <max@maxwofford.com>
2016-08-13 01:54:29 -0700

    Add email to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.provider = auth['provider']
-      user.uid = auth['uid']
-      user.name = auth['info']['name']
+      user.provider = auth[:provider]
+      user.uid      = auth[:uid]
+      user.name     = auth[:info][:name]
+      user.email    = auth[:info][:email]
     end
   end
 end
