a998c7bb1180ac84d92b00d0298fc663820cb308
Max Wofford <max@maxwofford.com>
2016-08-18 16:53:31 -0700

    Remove redundant 'begin' in workshops_controller

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -7,74 +7,66 @@ class WorkshopsController < ApplicationController
   end
 
   def handle_root_request
-    begin
-      request_path = params[:path]
-      request_path << ".#{params[:format]}" if params[:format]
-      path = workshops_path.join(request_path)
+    request_path = params[:path]
+    request_path << ".#{params[:format]}" if params[:format]
+    path = workshops_path.join(request_path)
 
-      file_exists = File.exist?(path)
-      is_workshop = File.directory?(path)
+    file_exists = File.exist?(path)
+    is_workshop = File.directory?(path)
 
-      if file_exists
-        if is_workshop
-          if trailing_slash?
-            render_workshop path
-          else
-            ensure_trailing_slash
-          end
+    if file_exists
+      if is_workshop
+        if trailing_slash?
+          render_workshop path
         else
-          deliver_file path
+          ensure_trailing_slash
         end
       else
-        raise ActionController::MissingFile
+        deliver_file path
       end
-    rescue Errno::ENOENT, ActionController::MissingFile
-      raise ActionController::RoutingError, "File Not Found"
+    else
+      raise ActionController::MissingFile
     end
+  rescue Errno::ENOENT, ActionController::MissingFile
+    raise ActionController::RoutingError, "File Not Found"
   end
 
   def render_workshop_file
-    begin
-      workshop = params[:workshop]
-      file = params[:file]
-      path = workshops_path.join(workshop, file)
+    workshop = params[:workshop]
+    file = params[:file]
+    path = workshops_path.join(workshop, file)
 
-      deliver_file path
-    rescue Errno::ENOENT, ActionController::MissingFile
-      raise ActionController::RoutingError, "File Not Found"
-    end
+    deliver_file path
+  rescue Errno::ENOENT, ActionController::MissingFile
+    raise ActionController::RoutingError, "File Not Found"
   end
 
   private
 
   def render_workshop(workshop_path)
-    begin
-      path = workshops_path.join(workshop_path, "README.md")
-      @title = File.basename(workshop_path).to_s.humanize.titleize
-      @metadata_page_name = @title
-      @metadata_page_category = "Workshop"
-      @metadata_should_not_track_page = true if signed_in?
+    path = workshops_path.join(workshop_path, "README.md")
+    @title = File.basename(workshop_path).to_s.humanize.titleize
+    @metadata_page_name = @title
+    @metadata_page_category = "Workshop"
+    @metadata_should_not_track_page = true if signed_in?
 
-      render_md_file path
+    render_md_file path
 
-      url = request.url
-      analytics.track_workshop_view(@title, url) if signed_in?
-    rescue Errno::ENOENT
-      raise ActionController::RoutingError, "Workshop Not Found"
-    end
+    url = request.url
+    analytics.track_workshop_view(@title, url) if signed_in?
+  rescue Errno::ENOENT
+    raise ActionController::RoutingError, "Workshop Not Found"
   end
 
   def deliver_file(path)
-    begin
-      if File.extname(path) == ".md"
-        @title = File.basename path
-        render_md_file path
-      else
-        send_file path
-      end
-    rescue Errno::ENOENT
-      raise ActionController::RoutingError, "File Not Found"
+    if File.extname(path) == ".md"
+      @title = File.basename path
+      render_md_file path
+    else
+      send_file path
     end
+  rescue Errno::ENOENT
+    raise ActionController::RoutingError, "File Not Found"
   end
 
   def render_md_file(path)
